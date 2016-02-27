a93380d5f6a92c771fa496e49d6d822bcd2d4f63
Slavik L <viacheslav.mail@gmail.com>
2016-02-27 12:48:45 +0200

    Edit task 7

--- task7.php
+++ task7.php
@@ -19,6 +19,8 @@ $_SESSION['authorized'] = isset($user_id);<br><br>
 Есть ли тут проблемы с безопасностью? И если да, то как обезопасить этот скрипт.<br><br>
 
 Мій варіант:<br>
+//запуск сесії<br>
+    session_start();<br>
 //перед записом інформації у змінні перевірка чи взагалі передавались дані з такими іменами<br>
 //передвати таку інформацію рекомендують через $_POST, хоча можна і $_REQUEST :)<br>
     if (isset($_POST['login'])) $login = $_POST['login'];<br>
@@ -27,24 +29,25 @@ $_SESSION['authorized'] = isset($user_id);<br><br>
 //потім перевірка чи поля не порожні<br>
     if (empty($login) or empty($password)) { // повідомлення про помилку; закінчення роботи скрипта }<br><br>
 
-//якщо не порожні, то обробляємо поля (екранування символів через \, перетворення спец символів і видалення пробілів) наприклад, для захисту від SQL-ін'єкцій<br>
-    $login = stripslashes(htmlspecialchars(trim($login)));<br>
-    $password = stripslashes(htmlspecialchars(trim($password)));<br><br>
+//пропоную так<br>
+    $login = mysqli_real_escape_string(htmlspecialchars(trim($login)));<br>
+    $password = mysqli_real_escape_string(htmlspecialchars(trim($password)));<br><br>
 
-//вважаємо, що підключення до БД налаштоване<br>
+//вважаємо, що підключення до БД налаштоване через об'єкт $mysqli, тоді<br>
 //робимо запит; змінюємо лапки на подвійні для всього запиту і одинарні для змінних<br>
-    $res = mysql_query("SELECT id FROM users WHERE login='$login'",$db);<br><br>
+    $res = $mysqli->query("SELECT `id` FROM `users` WHERE `login` = '$login'");<br><br>
 
 //перевіряємо успішність виконання самого запиту<br>
     if($res == 'TRUE'){<br><br>
-//якщо масив з результатом не пустий, то перевіряємо пароль, який для безпеки повинен зберігатися в БД не у чистому вигляді (а наприклад у вигляді хешу)<br>
-//не розумію використання в прикладі list() для однієї змінної.. можливо для зберігання всіх даних про юзера в одній змінній, але тоді в сесію потрапить і пароль, що не є правильно<br>
-    $row = mysql_fetch_array($res);<br>
+
+    //якщо масив з результатом не пустий, то перевіряємо пароль (я використовував станлартну функцію md5)<br>
+//відповідно значення паролю в БД має бути захешоване<br>
+    //fetch_array() використовую один раз без циклу бо приймаю, що значення шуканого id може бути лише одне або не бути зовсім
+    $row = $res->fetch_array();<br>
     if (!empty($row['password'])) {<br>
-        if($row['password'] == /*хеш-функція()*/($password)){<br>
+        if($row['password'] == md5($password)){<br>
             //зберігаємо значення логіна в сесії<br>
-            session_start();<br>
-            $_SESSION['authorized'] = $login; //або можна і $row['id']<br><br>
+            $_SESSION['login'] = $login;<br><br>
             //повідомлення про успішу авторизацію<br>
         }else{//помилка логіну або паролю}<br>
     }<br>
