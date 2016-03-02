949e029fc8151e3f87576802e69c5fe667b5f582
Viacheslav Liashuk <viacheslav.mail@gmail.com>
2016-03-02 16:42:35 +0200

    header update

--- header.php
+++ header.php
@@ -7,6 +7,8 @@ define('MAINFOLDER', '../');
 <html>
 <head>
     <link rel="stylesheet" href="<?=MAINFOLDER?>style.css" type="text/css" media="screen, projection" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="<?=MAINFOLDER?>script.js"></script>
 </head>
 <body>
 <div id="header">
@@ -14,6 +16,5 @@ define('MAINFOLDER', '../');
         <a href="<?=MAINFOLDER?>index.php"><li>Главная</li></a>
         <a href="<?=MAINFOLDER?>level1.php"><li>Уровень 1</li></a>
         <a href="<?=MAINFOLDER?>level2.php"><li>Уровень 2</li></a>
-        <a href="<?=MAINFOLDER?>level3.php"><li>Уровень 3</li></a>
     </ul>
 </div>
