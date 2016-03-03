3ed0cbabcf2e6f6ede4624f97a9c7b6221254a2f
Viacheslav Liashuk <viacheslav.mail@gmail.com>
2016-03-03 14:06:02 +0200

    task2_5_v1

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 $ = jQuery;
 $( document ).ready(function() {
     var count = 0;
-    $("input[type='button']").click(function() {
+
+    /*
+    селектори використав, але все одно прив'язано до кількості елементів...
+    тому що в завданні вказано конкрений порядок при натисканні.
+     */
+    $("#buttons input[type='button']").click(function() {
         switch (count) {
             case 0:
-                $("input[id='b1']").css("top", 90);
-                $("input[id='b2']").css("top", -30);
-                $("input[id='b3']").css("top", -30);
+                $("#buttons input:first-child").css("top", 90);
+                $("#buttons input:first-child").next().next().css("top", -30);
+                $("#buttons input:last-child").css("top", -30);
                 count++;
                 break;
             case 1:
-                $("input[id='b1']").css("top", 50);
-                $("input[id='b2']").css("top", 50);
-                $("input[id='b3']").css("top", -70);
+                $("#buttons input:first-child").css("top", 50);
+                $("#buttons input:first-child").next().next().css("top", 50);
+                $("#buttons input:last-child").css("top", -70);
                 count++;
                 break;
             case 2:
-                $("input[id='b1']").css("top", 10);
-                $("input[id='b2']").css("top", 10);
-                $("input[id='b3']").css("top", 10);
+                $("#buttons input:first-child").css("top", 10);
+                $("#buttons input:first-child").next().next().css("top", 10);
+                $("#buttons input:last-child").css("top", 10);
                 count = 0;
                 break;
         }
-
     });
 });
