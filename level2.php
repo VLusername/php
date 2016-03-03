f5bf5034f6c4d2d1a9adcf392a7d9bdbe6bfdf8c
Viacheslav Liashuk <viacheslav.mail@gmail.com>
2016-03-03 16:53:35 +0200

    task2_4

--- level2.php
+++ level2.php
@@ -5,6 +5,7 @@ include "header.php";
 <ul>
     <a href="<?=LEVEL2?>task2_1.php"><li>Задача 1</li></a>
     <a href="<?=LEVEL2?>task2_3.php"><li>Задача 3</li></a>
+    <a href="<?=LEVEL2?>task2_4.php"><li>Задача 4</li></a>
     <a href="<?=LEVEL2?>task2_5.php"><li>Задача 5</li></a>
 </ul>
 </div>
